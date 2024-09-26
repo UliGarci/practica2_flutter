3ce91313d63613538f4a99131277a5e708fe37aa
UliGarci <20203tn022@utez.edu.mx>
2024-09-25 20:17:01 -0600

    s

--- lib/widgets/splash_screen.dart
+++ lib/widgets/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:learning_b/widgets/home.dart';
 
 class SplashScreen extends StatelessWidget {
 const SplashScreen({ super.key });
@@ -6,49 +7,5 @@ const SplashScreen({ super.key });
   @override
   Widget build(BuildContext context){
     return home();
-  }
-}
-
-class home extends StatelessWidget {
-  const home({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16.0),
-      child: Row(
-        children: [
-          Padding(
-            padding: EdgeInsets.all(4.0),
-            child: Column(
-              children: [
-                Text('primer columna'),
-                Text('primer parrafo')
-              ],
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(4.0),
-            child: Column(
-              children: [
-                Text('segunda columna'),
-                Text('segundo parrafo')
-              ],
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(4.0),
-            child: Column(
-              children: [
-                Text('tercer columna'),
-                Text('tercer parrafo')
-              ],
-            ),
-          )
-        ],
-      ),
-    );
   }
 }
