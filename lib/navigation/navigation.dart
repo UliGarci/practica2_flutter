3a44e738a714a3676a054206364a45f38a7eb92b
UliGarci <20203tn022@utez.edu.mx>
2024-09-30 18:16:18 -0600

    navigationBar

---
+++ lib/navigation/navigation.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class navigation extends StatefulWidget {
+  const navigation({super.key});
+
+  @override
+  State<navigation> createState() => _navigationState();
+}
+
+class _navigationState extends State<navigation> {
+  int _selectedIndex =0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
