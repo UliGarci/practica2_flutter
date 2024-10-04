eaacd4d70a0c0433fdd6d3d8a8ff2a8b503820fd
UliGarci <20203tn022@utez.edu.mx>
2024-10-04 17:32:58 -0600

    Add. screen login

---
+++ lib/modules/auth/screens/login.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final TextEditingController _emailcontroller = TextEditingController();
+  final TextEditingController _passwordcontroller = TextEditingController();
+  bool _isObscure = true;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  String? validateEmail(String? value) {
+  // Expresión regular para validar un correo electrónico
+  final RegExp emailRegExp = RegExp(
+    r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
+  );
+
+  if (value == null || value.isEmpty) {
+    return 'Por favor, ingrese su correo electrónico';
+  } else if (!emailRegExp.hasMatch(value)) {
+    return 'Por favor, ingrese un correo electrónico válido';
+  }
+  return null; // Si es válido, no devuelve ningún error
+}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Inicio de sesión'),
+        centerTitle: true,
+        backgroundColor: const Color.fromRGBO(205, 55, 166,1),
+        titleTextStyle: const TextStyle(
+          fontSize: 16,
+          color: Colors.white
+        ),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset('assets/instagram3.webp',width: 200,height: 200),
+                const SizedBox(height: 16,),
+                TextFormField(
+                  validator: validateEmail,
+                  controller: _emailcontroller,
+                  decoration: const InputDecoration(
+                    hintText: 'Correo Electronico',
+                    label: Text('Correo Electronico')
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                const SizedBox(height: 32,),
+                TextFormField(
+                  controller: _passwordcontroller,
+                  obscureText: _isObscure,
+                  decoration: InputDecoration(
+                    hintText: 'Contraseña',
+                    label: const Text('Contraseña'),
+                    suffixIcon: IconButton(onPressed: (){
+                      setState(() {
+                        _isObscure = !_isObscure;
+                      });
+                    },
+                    icon: Icon(_isObscure ? Icons.visibility : Icons.visibility_off))
+                  ),
+                ),
+                const SizedBox(height: 50,),
+                SizedBox(
+                  height: 48,
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: (){
+                      if(_formKey.currentState!.validate()){
+                        print('Datos -> ${_emailcontroller.text} ${_passwordcontroller.text}');
+                      }
+                    },
+                    style: OutlinedButton.styleFrom(
+                      backgroundColor: Colors.blueAccent,
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16)
+                      )
+                    ),
+                    child: const Text('Iniciar sesión')
+                    ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
